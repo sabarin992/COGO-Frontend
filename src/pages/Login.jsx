2e875346342e6fcb942083e65edea64b7fad5cdd
Sabarinath E M <sabarinathem36@gmail.com>
2026-05-01 21:36:30 +0530

    refactor: update Login.jsx page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import api from '../api'
+import { toast } from 'react-toastify'
 
 const Login = () => {
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+
+    const handleLogin = async()=>{
+        try {
+            const res = await api.post("/auth/login",{email,password});
+            toast.success("Login successful!")
+            console.log(res.data);
+            
+        } catch (error) {
+            console.log(error.response);
+            
+        }
+    }
   return (
-    <div>Login</div>
+    <>
+
+        <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+        <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+        <button onClick={handleLogin}>Login</button>
+    </>
   )
 }
 
